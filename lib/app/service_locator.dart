fddd1f5ff48a1e8d54a0ec6eeb6c7fb0f2bf6646
Marcoslima016 <marcosvr.lima96@gmail.com>
2025-12-22 02:07:28 -0300

    feat [auth_login]: setup dependencies

--- lib/app/service_locator.dart
+++ lib/app/service_locator.dart
@@ -1,4 +1,7 @@
 import 'package:desafio_target_sistemas/lib.exports.dart';
+import 'package:desafio_target_sistemas/modules/auth/auth_core/external/datasources/auth_session.datasource.dart';
+import 'package:desafio_target_sistemas/modules/auth/auth_core/infra/repositories/auth_session_repository.dart';
+import 'package:desafio_target_sistemas/modules/auth/login/external/datasources/login_mock.datasource.dart';
 import 'package:get_it/get_it.dart';
 
 class ServiceLocator {
@@ -10,7 +13,48 @@ class ServiceLocator {
   T get<T extends Object>() => _getIt.get<T>();
 
   void setupDependencies() {
+    //
+    // ###### core #####
+
+    _getIt.registerFactory<ILocalStorageDriver>(() => SharedPreferencesDriver());
+
+    //
+    // ###### APP ######
+
     _getIt.registerLazySingleton<DesignSystem>(() => DesignSystem());
     _getIt.registerLazySingleton<AppNavigator>(() => AppNavigator(adapter: NativeNavigator()));
+
+    //
+    // ###### AUTH ######
+
+    // AUTH_CORE
+    _getIt.registerFactory<IAuthSessionDatasource>(
+      () => AuthSessionDatasource(driver: _getIt.get<ILocalStorageDriver>()),
+    );
+    _getIt.registerLazySingleton<IAuthSessionRepository>(
+      () => AuthSessionRepository(datasource: _getIt.get<IAuthSessionDatasource>()),
+    );
+    _getIt.registerFactory<IInitAuthSession>(
+      () => InitAuthSession(repository: _getIt.get<IAuthSessionRepository>()),
+    );
+
+    // LOGIN
+    _getIt.registerFactory<ILoginDatasource>(
+      () => LoginMockDatasource(),
+    );
+    _getIt.registerLazySingleton<ILoginRepository>(
+      () => LoginRepository(datasource: _getIt.get<ILoginDatasource>()),
+    );
+    _getIt.registerFactory<IGetUserDetails>(
+      () => GetUserDetails(loginRepository: _getIt.get<ILoginRepository>()),
+    );
+    _getIt.registerFactory<IExecuteLoginAttempt>(
+      () => ExecuteLoginAttempt(
+        loginRepository: _getIt.get<ILoginRepository>(),
+        getUserDetails: _getIt.get<IGetUserDetails>(),
+        initAuthSession: _getIt.get<IInitAuthSession>(),
+      ),
+    );
+    _getIt.registerSingleton<LoginStore>(LoginStore());
   }
 }
